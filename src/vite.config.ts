f57c6f8bb8859eeef91ac2770b04bc2932d0c121
MasoStefano4 <masostefano4@gmail.com>
2026-05-06 12:02:42 +0200

    Update vite.config.ts

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/var/www/html/resources/ts',
+      '@': new URL('./resources/ts', import.meta.url).pathname,
     },
   },
   test: {
