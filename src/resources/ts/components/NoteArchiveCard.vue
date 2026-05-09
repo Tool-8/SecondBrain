d4305dbfdf375f3198887154df59b0b5f5ddef78
NicoFeltrin <niccolo.feltrin.kennedy@gmail.com>
2026-05-09 11:32:07 +0200

    Fix NoteArchiveCard da <a> a <RouterLink>

--- src/resources/ts/components/NoteArchiveCard.vue
+++ src/resources/ts/components/NoteArchiveCard.vue
@@ -9,7 +9,7 @@ defineProps<{
 
 <template>
     <li>
-        <a :href="'/notes/' + id || '#'">
+        <RouterLink :to="'/notes/' + id || '#'">
             <div class="px-6 py-4 rounded shadow-xs border border-gray-200 hover:border-blue-500 hover:bg-blue-500/10 dark:border-neutral-800 dark:hover:border-blue-400 dark:hover:bg-blue-400/10">
                 <div class="flex flex-col gap-2">
                     <p class="font-bold text-xl dark:text-neutral-200">{{ name }}</p>
@@ -19,6 +19,6 @@ defineProps<{
                     </div>
                 </div>
             </div>
-        </a>
+        </RouterLink>
     </li>
 </template>
