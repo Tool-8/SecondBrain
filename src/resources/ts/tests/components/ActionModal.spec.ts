b72deb50baa4d92f05449804b187a424ad48ed4a
MasoStefano4 <masostefano4@gmail.com>
2026-05-14 15:47:14 +0200

    Update ActionModal.spec.ts

--- src/resources/ts/tests/components/ActionModal.spec.ts
+++ src/resources/ts/tests/components/ActionModal.spec.ts
@@ -34,4 +34,35 @@ describe('ActionModal', () =>  {
 
         expect(resolveMock).toHaveBeenCalledWith('Nuovo nome')
     }) 
+
+    it('rename: should resolve with null value', async () => {
+        const resolveMock = vi.fn();
+
+        const wrapper = mount(ActionModal, {
+            global: {
+                stubs: {
+                RenamePromise: {
+                    template: `<div><slot :resolve="resolve" :args="args" /></div>`,
+                    props: ['resolve', 'args'],
+                    setup(props) {
+                    return { 
+                        resolve: resolveMock, 
+                        args: ['Nota Originale'] 
+                    };
+                    }
+                },
+                BaseModal: {
+                    template: ' <div> <slot name="body" /> <slot name="footer" /> </div>' 
+                }
+                }
+            }
+            });
+
+        const button = wrapper.findAll('button').find(b => b.text() === 'Annulla')
+        await button?.trigger('click')
+
+        expect(resolveMock).toHaveBeenCalledWith(null)
+    }) 
+
+
 })
