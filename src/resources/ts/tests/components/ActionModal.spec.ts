fa0b5718e285e2b23d53b773c18ff0e600fb6222
MasoStefano4 <masostefano4@gmail.com>
2026-05-14 15:51:47 +0200

    Update ActionModal.spec.ts

--- src/resources/ts/tests/components/ActionModal.spec.ts
+++ src/resources/ts/tests/components/ActionModal.spec.ts
@@ -64,5 +64,65 @@ describe('ActionModal', () =>  {
         expect(resolveMock).toHaveBeenCalledWith(null)
     }) 
 
+    it('saveAs: should resolve with input value', async () => {
+        const resolveMock = vi.fn();
+
+        const wrapper = mount(ActionModal, {
+            global: {
+                stubs: {
+                SaveAsPromise: {
+                    template: `<div><slot :resolve="resolve" :args="args" /></div>`,
+                    props: ['resolve', 'args'],
+                    setup(props) {
+                    return { 
+                        resolve: resolveMock, 
+                        args: ['Nota Originale'] 
+                    };
+                    }
+                },
+                BaseModal: {
+                    template: ' <div> <slot name="body" /> <slot name="footer" /> </div>' 
+                }
+                }
+            }
+            });
+
+        const input = wrapper.find('input');
+        await input.setValue('Nuovo nome');
+
+        const button = wrapper.findAll('button').find(b => b.text() === 'OK')
+        await button?.trigger('click')
+
+        expect(resolveMock).toHaveBeenCalledWith('Nuovo nome')
+    }) 
+
+    it('rename: should resolve with null value', async () => {
+        const resolveMock = vi.fn();
+
+        const wrapper = mount(ActionModal, {
+            global: {
+                stubs: {
+                SaveAsPromise: {
+                    template: `<div><slot :resolve="resolve" :args="args" /></div>`,
+                    props: ['resolve', 'args'],
+                    setup(props) {
+                    return { 
+                        resolve: resolveMock, 
+                        args: ['Nota Originale'] 
+                    };
+                    }
+                },
+                BaseModal: {
+                    template: ' <div> <slot name="body" /> <slot name="footer" /> </div>' 
+                }
+                }
+            }
+            });
+
+        const button = wrapper.findAll('button').find(b => b.text() === 'Annulla')
+        await button?.trigger('click')
+
+        expect(resolveMock).toHaveBeenCalledWith(null)
+    }) 
 
 })
